9328692ccbb1a62941eeff9c0a23ce44f53be0e2
mmiyaji <ruhenheim1223@gmail.com>
2026-06-29 03:37:56 +0900

    fix: improve editor automation names

--- IconSonic/MainPage.xaml.cs
+++ IconSonic/MainPage.xaml.cs
@@ -50,6 +50,16 @@ public sealed partial class MainPage : Page
         return new SolidColorBrush(color);
     }
 
+    public static string IncludeFrameAutomationName(string sizeLabel)
+    {
+        return $"Include {sizeLabel} in export";
+    }
+
+    public static string PaletteColorAutomationName(Color color)
+    {
+        return $"Use palette color #{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+    }
+
     private async void OpenImageButton_Click(object sender, RoutedEventArgs e)
     {
         FileOpenPicker picker = new()
